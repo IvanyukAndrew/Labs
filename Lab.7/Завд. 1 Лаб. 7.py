e740fa561da7806f808bc81da790eb7481eb0a43
IvanyukAndrew <73875988+IvanyukAndrew@users.noreply.github.com>
2020-12-14 17:14:01 +0200

    Update Завд. 1 Лаб. 7.py

--- Lab.7/Завд. 1 Лаб. 7.py
+++ Lab.7/Завд. 1 Лаб. 7.py
@@ -1,21 +1,13 @@
-R = int(input("Кількість рядків R = "))
-E = int(input("Кількість елементів E = "))
+coulmn = int(input("coulmn: "))
+row = int(input("row: "))
 
 import random
-a = [[random.randint(-100,100) for j in range(E)] for i in range(R)]
+a = [[random.randint(-10,10) for j in range(row)] for i in range(coulmn)]
 
-b = []
+S = 0
 
-for row in a:
-    row_str=['{0:5d}'.format(el) for el in row]
-    b.append(row_str)
-
-b = [''.join(['{0:5d}'.format(el) for el in row]) for row in a]
-print(*b,sep='\n')
-
-s = 0
-for i in range(1,R,2):
-    for j in range(1,E,2):
-        if a[i][j]<0:
-            s*=a[i][j]
-print("Cума відємних елементів матриці з обома непарними індексами= {0}". format(s))
+for i in range(coulmn):
+    for j in range(row):
+        if i % 2 == 1 and j % 2 == 1 and a[i][j] < 0:
+            S = S * a[i][j]
+print("Добуток відємних елементів матриці з обома непарними індексами: {0}". format(S))
