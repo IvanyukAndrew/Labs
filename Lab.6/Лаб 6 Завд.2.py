290d95907f42fd105eff6faa73d8f88c7ff563b8
IvanyukAndrew <73875988+IvanyukAndrew@users.noreply.github.com>
2020-12-20 19:20:45 +0200

    Update Лаб 6 Завд.2.py

--- Lab.6/Лаб 6 Завд.2.py
+++ Lab.6/Лаб 6 Завд.2.py
@@ -2,19 +2,15 @@ import math
 
 n=int(input("Введіть кількість елементів n: "))
 a = []
-i=1
-for i in range(n):
-    if i<=n:
-        a += [(-1+2-3+(-1)**i*i)/(math.factorial(i))]
-        i=i+1
+b = 0
+for i in range(1, n+1):
+    b = b + ((-1)**i)*i
+    a.append(b/math.factorial(i))
 
-A=a
 print("Масив A={0}".format(a))
 
 S = 0
-for i in range(n):
-    if a[i]>0:
-        S=S+a[i]
+for el in a:
+    if el>0:
+        S=S+el
 print("Сума додатніх елементів масиву А = {0}".format(S))
-
-
