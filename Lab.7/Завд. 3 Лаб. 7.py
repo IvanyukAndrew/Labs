307c0511af91762109c27809bd102b269208d175
IvanyukAndrew <73875988+IvanyukAndrew@users.noreply.github.com>
2020-12-20 19:28:20 +0200

    Update Завд. 3 Лаб. 7.py

--- Lab.7/Завд. 3 Лаб. 7.py
+++ Lab.7/Завд. 3 Лаб. 7.py
@@ -3,11 +3,16 @@ n = int(input("Розмір матриці А і В: "))
 import random
 A = [[random.randint(-10,10) for j in range(n)]for i in range(n)]
 B = [[random.randint(-10,10) for j in range(n)]for i in range(n)]
+C = [[x+y for x,y in zip(i,j)]for i,j in zip(A,B)]
 
-print("A =",A)
-print("B =",B)
+print("Матриця A: ")
+for i in A:
+    print(i)
 
-C = A + B
+print("Матриця B: ")
+for i in B:
+    print(i)
 
-print("C =",C)
-
+print("Матриця C: ")
+for i in C:
+    print(i)
